d5d8ef1b710c4d254690a1bf790660d1645fce76
高志刚 <gaozhigang@gaozhigangdeMacBook-Air.local>
2015-05-20 22:08:56 +0800

    Initial Commit

---
+++ lesson1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  lesson1
+//
+//  Created by 高志刚 on 15/5/20.
+//  Copyright (c) 2015年 高志刚. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
